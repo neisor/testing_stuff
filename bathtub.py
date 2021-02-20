5693e9ecfd8986ca54f081497ace26072af2a7b4
Antonio Raffaele Iannaccone <antonio.iannaccone7@gmail.com>
2021-02-20 09:06:43 +0100

    Added filling up for Bathtub & test for fill_up()

--- bathtub.py
+++ bathtub.py
@@ -1,6 +1,12 @@
+import time
 class Bathtub():
     def __init__(self, color: str, water_capacity: int, shape: str):
         self.color = color
         self.water_capacity = water_capacity
         self.shape = shape
-        
+    
+    def fill_up(self):
+        print(f'Your bathtub is filling up. {self.water_capacity} liters of water are being poured in...', end='\r')
+        time.sleep(5)
+        print('Congratulations! Your bathtub has been filled up. You can take your bath now.')
+        return "Filled up"
--- bathtub_tests.py
+++ bathtub_tests.py
@@ -1,8 +1,11 @@
 from bathtub import Bathtub
 
+bathtub = Bathtub('white', 100, 'oval')
+
 def test_check_variable_types():
-    bathtub = Bathtub('white', 100, 'oval')
     assert type(bathtub.color) == str
     assert type(bathtub.water_capacity) == int
     assert type(bathtub.shape) == str
 
+def test_fill_up():
+    assert bathtub.fill_up() == "Filled up"
