4a48cd9f0d8c16cbf16ff97ffcd24ca6f79ff583
Antonio Raffaele Iannaccone <antonio.iannaccone7@gmail.com>
2021-02-20 09:01:15 +0100

    added bathtub.py with initial Bathtub class

---
+++ bathtub.py
@@ -0,0 +1,6 @@
+class Bathtub():
+    def __init__(self, color: str, water_capacity: int, shape: str):
+        self.color = color
+        self.water_capacity = water_capacity
+        self.shape = shape
+        
