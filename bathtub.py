32e01e3bca39f225ccb6063ff06a78265d05e656
Antonio Raffaele Iannaccone <antonio.iannaccone7@gmail.com>
2021-02-20 09:24:59 +0100

    Modify Bathtub to ask user for properties -> input

--- bathtub.py
+++ bathtub.py
@@ -1,9 +1,12 @@
 import time
 class Bathtub():
-    def __init__(self, color: str, water_capacity: int, shape: str):
-        self.color = color
-        self.water_capacity = water_capacity
-        self.shape = shape
+    def __init__(self):
+        print("What should the color of the bathtub be?:")
+        self.color = input()
+        print("What should the capacity of the bathtub be?:")
+        self.water_capacity = int(input())
+        print("What should the shape of the bathtub be?:")
+        self.shape = input()
     
     def fill_up(self):
         print(f'Your bathtub is filling up. {self.water_capacity} liters of water are being poured in...', end='\r')
