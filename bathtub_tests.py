e35fa541389c9a71e0873e98526734fcbcd73f6f
Antonio Raffaele Iannaccone <antonio.iannaccone7@gmail.com>
2021-02-20 09:47:31 +0100

    added new test to check if bathtub is filled up

--- bathtub_tests.py
+++ bathtub_tests.py
@@ -6,6 +6,10 @@ def test_check_variable_types():
     assert type(bathtub.color) == str
     assert type(bathtub.water_capacity) == int
     assert type(bathtub.shape) == str
+    assert bathtub.status == "empty"
 
 def test_fill_up():
     assert bathtub.fill_up() == "Filled up"
+
+def test_check_fill():
+    assert bathtub.status == "filled"
